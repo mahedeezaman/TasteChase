e276f51dfeb6e9b73bdd8c905fc073e8ee2609ea
Kobiraj <mahedeezaman@iut-dhaka.edu>
2024-09-12 11:33:54 -0400

    Initial Commit

---
+++ TasteChase/TasteChaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteChaseApp.swift
+//  TasteChase
+//
+//  Created by Kobiraj on 2024-09-12.
+//
+
+import SwiftUI
+
+@main
+struct TasteChaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
