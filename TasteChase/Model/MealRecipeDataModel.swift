ebdf175d88ff805dfbd681cac512b3a5315d06df
Kobiraj <mahedeezaman@iut-dhaka.edu>
2024-09-13 08:12:06 -0400

    Updating model

--- TasteChase/Model/MealRecipeDataModel.swift
+++ TasteChase/Model/MealRecipeDataModel.swift
@@ -8,13 +8,17 @@
 import Foundation
 
 struct MealRecipeDataModel {
-    var idMeal, strMeal, strDrinkAlternate, strCategory: String
-    var strArea, strInstructions: String
-    var strTags: String
-    var strYoutube: String
-    var ingredientAndMeasure : [(String, String)]
-    var strSource: String
-    var strImageSource: String
-    var strCreativeCommonsConfirmed: String
-    var dateModified: String
+    var idMeal : String = ""
+    var strMeal : String = ""
+    var strDrinkAlternate : String = ""
+    var strCategory: String = ""
+    var strArea : String = ""
+    var strInstructions: String = ""
+    var strTags: String = ""
+    var strYoutube: String = ""
+    var ingredientAndMeasure : [(String, String)] = []
+    var strSource: String = ""
+    var strImageSource: String = ""
+    var strCreativeCommonsConfirmed: String = ""
+    var dateModified: String = ""
 }
