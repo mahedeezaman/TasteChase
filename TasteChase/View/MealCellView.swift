6bbea28f548332bc53ab22a41aca3895e19e172f
Kobiraj <mahedeezaman@iut-dhaka.edu>
2024-09-12 22:22:53 -0400

    Implemented async await for image fetching

--- TasteChase/View/MealCellView.swift
+++ TasteChase/View/MealCellView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct MealCellView: View {
-    let meal: Meal
+    let meal: MealViewDataModel
     @EnvironmentObject var mealVM: MealViewModel
     
     var body: some View {
         HStack {
-            if let imageUrl = mealVM.loadImage(from: meal.strMealThumb ?? "") {
-                Image(uiImage: imageUrl)
+            if let image = meal.urlImage {
+                Image(uiImage: image)
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 80, height: 80)
@@ -23,19 +23,25 @@ struct MealCellView: View {
                 ProgressView()
                     .frame(width: 80, height: 80)
             }
-            Text(meal.strMeal ?? "")
+            Text(meal.strMeal)
             Spacer()
         }
         .padding()
+        .onAppear {
+            mealVM.loadImage(for: meal)
+        }
+        .onDisappear {
+            mealVM.cancelImageFetch(for: meal)
+        }
     }
 }
 
 #Preview {
-    MealCellView(meal:
-        Meal(
+    MealCellView(
+        meal: MealViewDataModel(
+            idMeal: "52768",
             strMeal: "Apple Frangipan Tart",
-            strMealThumb: "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
-            idMeal: "52768"
+            strMealThumb: "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg"
         )
     )
     .environmentObject(MealViewModel())
--- TasteChase/ViewModel/MealViewModel.swift
+++ TasteChase/ViewModel/MealViewModel.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 class MealViewModel: ObservableObject {
-    @Published var meals: [Meal] = []
-    private var imageCache: [String: UIImage] = [:]
+    @Published var meals: [MealViewDataModel] = []
     
     func fetchMeals() async {
         do {
-            let responseData = try await NetworkServices.performRequestOperation(url: NetworkConstants.mealUrl, responseType: MealDataModel.self)
+            let responseData = try await NetworkServices.performRequestOperation(url: NetworkConstants.mealUrl, responseType: MealResponseModel.self)
             if let mealsData = responseData.meals {
                 DispatchQueue.main.async {
                     for meal in mealsData {
-                        let data = Meal(strMeal: meal.strMeal ?? "", strMealThumb: meal.strMealThumb ?? "", idMeal: meal.idMeal ?? "")
+                        let data = MealViewDataModel(
+                            idMeal: meal.idMeal ?? "",
+                            strMeal: meal.strMeal ?? "",
+                            strMealThumb: meal.strMealThumb ?? "",
+                            urlImage: nil
+                        )
                         if data.idMeal != "" {
                             self.meals.append(data)
                         }
@@ -29,18 +33,36 @@ class MealViewModel: ObservableObject {
         }
     }
     
-    func loadImage(from url: String) -> UIImage? {
-        if let cachedImage = imageCache[url] {
-            return cachedImage
-        }
-        guard let imageUrl = URL(string: url),
-              let data = try? Data(contentsOf: imageUrl),
-              let image = UIImage(data: data) else {
-            return nil
+    func loadImage(for meal: MealViewDataModel) {
+        guard let index = meals.firstIndex(where: { $0.idMeal == meal.idMeal }) else {
+            return
         }
         
-        DispatchQueue.main.async {
-            self.imageCache[url] = image
+        meals[index].fetchTask?.cancel()
+        
+        meals[index].fetchTask = Task {
+            if let image = await fetchImage(from: meal.strMealThumb) {
+                DispatchQueue.main.async {
+                    self.meals[index].urlImage = image
+                }
+            }
+        }
+    }
+
+    func cancelImageFetch(for meal: MealViewDataModel) {
+        guard let index = meals.firstIndex(where: { $0.idMeal == meal.idMeal }) else {
+            return
+        }
+        meals[index].fetchTask?.cancel()
+        meals[index].fetchTask = nil
+    }
+
+    private func fetchImage(from url: String) async -> UIImage? {
+        try? await Task.sleep(nanoseconds: 500_000_000) // Simulate network delay
+        guard let url = URL(string: url),
+              let data = try? Data(contentsOf: url),
+              let image = UIImage(data: data) else {
+            return nil
         }
         return image
     }
