ee1b88afd431224a24a123d2309a9bfc22d0fed5
Kobiraj <mahedeezaman@iut-dhaka.edu>
2024-09-13 22:27:18 -0400

    Created YoutubeVideoPlayer view component

---
+++ TasteChase/View/ViewComponents/YoutubeVideoPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  YoutubeVideoPlayer.swift
+//  TasteChase
+//
+//  Created by Kobiraj on 2024-09-13.
+//
+
+import SwiftUI
+import WebKit
+
+struct YouTubeView: UIViewRepresentable {
+    let youTubeurl: String
+    func makeUIView(context: Context) ->  WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let demoURL = URL(string: youTubeurl) else { return }
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: demoURL))
+    }
+}
