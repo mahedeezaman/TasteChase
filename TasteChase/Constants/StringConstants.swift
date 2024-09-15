989a92890b6a69f49eb5397830847796c79c804f
Kobiraj <mahedeezaman@iut-dhaka.edu>
2024-09-15 15:04:05 -0400

    Updated RecipeView

--- TasteChase/Constants/StringConstants.swift
+++ TasteChase/Constants/StringConstants.swift
@@ -9,4 +9,11 @@ import Foundation
 
 struct StringConstants {
     static let selectMeal = "Select a Meal for Recipe"
+    static let ingredients = "Ingredients"
+    static let method = "Method"
+    static let step = "• Step"
+    static let source = "Source: "
+    static let imageSource = "Image Source: "
+    static let ccConfirmed = "Creative Commons Confirmed: "
+    static let dateModified = "Date Modified: "
 }
--- TasteChase/View/MealRecipeView.swift
+++ TasteChase/View/MealRecipeView.swift
@@ -13,100 +13,160 @@ struct MealRecipeView: View {
     
     var body: some View {
         ZStack {
-            if mealRecipeVM.isLoading {
-                ProgressView()
-            }
             ScrollView {
                 VStack(alignment: .leading, spacing: 20){
                     HStack {
                         Spacer()
-                        VStack {
+                        VStack(spacing: 15) {
                             if let image = meal.urlImage {
                                 Image(uiImage: image)
                                     .resizable()
                                     .aspectRatio(contentMode: .fill)
-                                    .frame(width: 300, height: 300)
+                                    .frame(width: 275, height: 275)
                                     .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(Color(fromHex: ColorConstants.primaryText), lineWidth: 2)
+                                    )
                             } else {
-                                ProgressView()
-                                    .frame(width: 80, height: 80)
+                                progressRing()
+                                    .frame(width: 275, height: 275)
+                                    .cornerRadius(20)
                             }
                             Text(meal.strMeal)
+                                .font(.custom(CeraProFontFamily.black, size: 25))
+                                .foregroundStyle(Color(fromHex: ColorConstants.title))
                         }
                         Spacer()
                     }
                     
                     VStack(alignment: .leading, spacing: 10) {
                         if !mealRecipeVM.mealsRecipe.strDrinkAlternate.isEmpty {
-                            Text("Drink Alternate: \(mealRecipeVM.mealsRecipe.strDrinkAlternate)")
+                            Text("Drink Alternate: ")
+                                .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                            
+                            Text("\(mealRecipeVM.mealsRecipe.strDrinkAlternate)")
+                                .font(.custom(CeraProFontFamily.light, size: 18))
                         }
                         if !mealRecipeVM.mealsRecipe.strCategory.isEmpty {
-                            Text("Category: \(mealRecipeVM.mealsRecipe.strCategory)")
+                            Text("Category: ")
+                                .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                            
+                            Text("\(mealRecipeVM.mealsRecipe.strCategory)")
+                                .font(.custom(CeraProFontFamily.light, size: 18))
                         }
                         if !mealRecipeVM.mealsRecipe.strTags.isEmpty {
-                            Text("Tags: \(mealRecipeVM.mealsRecipe.strTags)")
+                            Text("Tags: ")
+                                .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                            
+                            Text("\(mealRecipeVM.mealsRecipe.strTags)")
+                                .font(.custom(CeraProFontFamily.light, size: 18))
                         }
                         if !mealRecipeVM.mealsRecipe.strArea.isEmpty {
-                            Text("Area: \(mealRecipeVM.mealsRecipe.strArea)")
+                            Text("Area: ")
+                                .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                            
+                            Text("\(mealRecipeVM.mealsRecipe.strArea)")
+                                .font(.custom(CeraProFontFamily.light, size: 18))
                         }
                     }
+                    .foregroundStyle(Color(fromHex: ColorConstants.primaryText))
                     .padding(.vertical)
                     
-                    Text("Ingredients")
+                    Text(StringConstants.ingredients)
+                        .font(.custom(CeraProFontFamily.bold, size: 20))
+                        .foregroundStyle(Color(fromHex: ColorConstants.title))
+                    
                     VStack(alignment: .leading, spacing: 10) {
                         ForEach(mealRecipeVM.mealsRecipe.ingredientAndMeasure.indices, id: \.self) { index in
                             let ingAndMeas = mealRecipeVM.mealsRecipe.ingredientAndMeasure[index]
                             Text("\(ingAndMeas.0) : \(ingAndMeas.1)")
+                                .font(.custom(CeraProFontFamily.medium, size: 18))
+                                .foregroundStyle(Color(fromHex: ColorConstants.primaryText))
                                 .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color.red, lineWidth: 0.7)
-                                )
+                                .background(Color(fromHex: ColorConstants.cell))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
                                 .padding(.horizontal)
                         }
                     }
                     
-                    Text("Method")
+                    Text(StringConstants.method)
+                        .font(.custom(CeraProFontFamily.bold, size: 20))
+                        .foregroundStyle(Color(fromHex: ColorConstants.title))
+                    
                     VStack(alignment: .leading, spacing: 10) {
                         ForEach(0..<mealRecipeVM.mealsRecipe.strInstructions.count, id: \.self) { ind in
-                            Text("• Step \(ind+1): \(mealRecipeVM.mealsRecipe.strInstructions[ind]).")
-                                .padding(.horizontal)
+                            Text("\(StringConstants.step) \(ind+1): ")
+                                .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                            
+                            Text("\(mealRecipeVM.mealsRecipe.strInstructions[ind]).")
+                                .font(.custom(CeraProFontFamily.light, size: 18))
                         }
                     }
+                    .foregroundStyle(Color(fromHex: ColorConstants.primaryText))
+                    .padding(.horizontal)
                     
                     if !mealRecipeVM.mealsRecipe.strYoutube.isEmpty {
-                        YouTubeView(youTubeurl: mealRecipeVM.mealsRecipe.strYoutube)
-                            .frame(width: 300, height: 300)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(Color.red, lineWidth: 0.7)
-                            )
-                            .cornerRadius(20)
-                            .padding()
+                        HStack {
+                            Spacer()
+                            YouTubeView(youTubeurl: mealRecipeVM.mealsRecipe.strYoutube)
+                                .frame(width: 275, height: 275)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color(fromHex: ColorConstants.cell), lineWidth: 0.7)
+                                )
+                                .cornerRadius(20)
+                                .padding()
+                            Spacer()
+                        }
                     }
                     
                     VStack(alignment: .leading) {
                         if let url = URL(string: mealRecipeVM.mealsRecipe.strSource), !mealRecipeVM.mealsRecipe.strSource.isEmpty {
                             HStack(alignment: .top) {
-                                Text("Source: ")
+                                Text(StringConstants.source)
+                                    .font(.custom(CeraProFontFamily.bold, size: 18))
+                                
                                 Link(mealRecipeVM.mealsRecipe.strSource, destination: url)
+                                    .font(.custom(CeraProFontFamily.bold, size: 18))
+                                    .lineLimit(1)
                             }
                         }
-                        if !mealRecipeVM.mealsRecipe.strImageSource.isEmpty {
-                            Text("Image Source: \(mealRecipeVM.mealsRecipe.strImageSource)")
+                        if let url = URL(string: mealRecipeVM.mealsRecipe.strImageSource), !mealRecipeVM.mealsRecipe.strImageSource.isEmpty {
+                            HStack(alignment: .top) {
+                                Text(StringConstants.imageSource)
+                                    .font(.custom(CeraProFontFamily.bold, size: 18))
+                                
+                                Link(mealRecipeVM.mealsRecipe.strImageSource, destination: url)
+                                    .font(.custom(CeraProFontFamily.bold, size: 18))
+                            }
                         }
                         if !mealRecipeVM.mealsRecipe.strCreativeCommonsConfirmed.isEmpty {
-                            Text("Creative Commons Confirmed: \(mealRecipeVM.mealsRecipe.strCreativeCommonsConfirmed)")
+                            Text(StringConstants.ccConfirmed)
+                                .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                            
+                            Text("\(mealRecipeVM.mealsRecipe.strCreativeCommonsConfirmed)")
+                                .font(.custom(CeraProFontFamily.light, size: 18))
                         }
                     }
+                    
                     if !mealRecipeVM.mealsRecipe.dateModified.isEmpty {
-                        Text("Date Modified: \(mealRecipeVM.mealsRecipe.dateModified)")
+                        Text(StringConstants.dateModified)
+                            .font(.custom(CeraProFontFamily.bold, size: 18)) +
+                        
+                        Text("\(mealRecipeVM.mealsRecipe.dateModified)")
+                            .font(.custom(CeraProFontFamily.light, size: 18))
                     }
                 }
-                .padding()
+                .padding(20)
                 .padding(.vertical, 10)
             }
             .disabled(mealRecipeVM.isLoading)
+            if mealRecipeVM.isLoading {
+                BlurView()
+                    .opacity(0.1)
+                progressRing()
+            }
         }
         .background(Color(fromHex: ColorConstants.background))
         .navigationBarTitle("", displayMode: .inline)
